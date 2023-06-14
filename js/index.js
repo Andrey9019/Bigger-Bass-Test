0f9c3bd96eb6385a4e0f350a286b5542244612dd
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-06-14 20:52:14 +0300

    ,

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// const age = `25px`;
+// console.log(age)
+
+// const totalPrise =  200.74
+// console.log(`цена`, totalPrise)
+
+// let age = prompt('введи знач')
+// age = Number(age)
+// console.log(age)
+
+
+
+// let base = prompt(`давай число`);
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt(`давай ступень`);
+// power = Number(power);
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result)
+
+
+
+// const room = 38;
+// const type = 'VIP';
+// const firstName = 'Andrey';
+// const lastName = 'Zircdenko';
+
+// const welcomeMsg =`Гость ${firstName} ${lastName} поселяється в ${type} номер ${room}`
+
+// console.log(welcomeMsg)
+
+
+// const x1 = 10;
+// const x2 = 30;
+// const number = 20;
+
+// console.log(`число ${number} попадает в отрезок до ${x1}`, number < x1);
+// console.log(`число ${number} попадает в отрезок после ${x2}`, number > x2);
+// console.log(`число ${number} попадает в отрезок от ${x1} до ${x2}`,
+//     number > x1 || number < x2);
+
+
+
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log(`Можно открыть чат?` , canOpenChat)
+
+
+
+// if (1) {
+//     console.log(`true)`)
+// }
+
+
+// if (1) {
+//     console.log(`true)`)
+// } else {
+//     console.log(`false)`)
+// }
+
---
+++ js/module-js/module-01.js
@@ -0,0 +1,90 @@
+// 1
+// const productName = "Droid";
+// const pricePerItem = 2000;
+ 
+// 2
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = 'Repair droid';
+// pricePerItem = 3500;
+
+// 3
+// const topSpeed = 160;
+// const distance  = 617.54;
+// const login  = 'mango935';
+// const isOnline  = true;
+// const isAdmin = false;
+
+// 4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice)
+
+// 5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
+
+// 6
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+// console.log(message)
+// console.log(totalPrice)
+
+// 7
+// function sayHi() {
+//     console.log("Hello, this is my first function!");}
+// sayHi();
+
+// 8
+// function add(a,b,c) {console.log(`Addition result equals ${a + b + c}`);}
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// 9
+// function add(a, b, c) {return a + b + c;}
+// add(2, 5, 8); // 15
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// 10
+// function makeMessage (name, price) {
+//     const message = `You picked ${name}, price per item is ${price} credits` ;
+//    return message;};
+
+// 11
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//     const totalPrice = orderedQuantity * pricePerItem;
+//   return totalPrice;};
+
+//12
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//     const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//     const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+//   return message;}
+
+//13
+// function isAdult(age) {
+//     passed = age >= 18;
+//     return passed;}
+// userAge = 20;
+// result = isAdult(userAge);
+// console.log(result);
+
+//14
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+
+//   const isMatch = ;
+
+
+//   return isMatch;
+// }
