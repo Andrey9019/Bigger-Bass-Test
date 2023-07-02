4eff434dd3aeedc2d7f096280c3740d010b21fa8
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-07-03 01:13:41 +0300

    2

--- js/task/task-03.js
+++ js/task/task-03.js
@@ -99,3 +99,92 @@
 //   poly: 50,
 //   ajax: 150,
 // });
+
+// Task-18
+
+// const products = [
+//   { name: "Grip", price: 1200, quantity: 9 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Droid", price: 400, quantity: 7 },
+// ];
+
+// function getProductPrice(productName) {
+//   for (let product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+// }
+// getProductPrice("Radar");
+// getProductPrice("Grip");
+// getProductPrice("Engine");
+
+// Task-19
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+// function getAllPropValues(propName) {
+//   let value = [];
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       value.push(product[propName]);
+//     }
+//   }
+//   return value;
+// }
+
+// getAllPropValues("quantity");
+// getAllPropValues("price");
+// getAllPropValues("category");
+
+// Task-20
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   let totalPrice = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       totalPrice = product.price * product.quantity;
+//     }
+//   }
+//   //   return totalPrice;
+//   console.log(totalPrice);
+// }
+
+// calculateTotalPrice("Blaster");
+// calculateTotalPrice("Radar");
+// calculateTotalPrice("Droid");
+// calculateTotalPrice("Grip");
+
+// Task-23
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+// const highYesterday = highTemperatures.yesterday;
+// const highToday = highTemperatures.today;
+// const highTomorrow = highTemperatures.tomorrow;
+// const highIcon = highTemperatures.icon;
+
+const {
+  highIcon:
+    firstCoverImage = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+} = highTemperatures;
+
+// Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
