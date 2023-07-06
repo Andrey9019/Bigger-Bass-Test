681c9e39ce1caef48b2ff336ab21009d945cf110
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-07-07 00:57:31 +0300

    Update module-04.js

--- js/module-js/module-04.js
+++ js/module-js/module-04.js
@@ -10,3 +10,52 @@
 // registerGuest("Полі", function greet(name) {
 //   console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
 // });
+
+// const arrow = () => {
+//   console.log(`hello`);
+// };
+// arrow();
+
+// const sum = (a) => a + 3;
+// console.log(sum(2));
+
+// const foo = (array) => console.log(array);
+// foo([1, 2, 3]);
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(2, 3));
+// console.log(add);
+
+// const arr = [1, 2, 3, 4, 5];
+// function add(...args) {
+//   console.log(`add`, args);
+// }
+// function sum(...args) {
+//   console.log(`sum`, args);
+// }
+// function foo(value, callback) {
+//   callback(...value);
+// }
+
+// foo(arr, add);
+// foo(arr, sum);
+
+// forEach
+
+// const arr = [1, 2, 3, 4];
+// arr.forEach((elem) => {
+//   console.log(elem);
+// });
+
+// Task-1
+
+// const logItem = (arr) =>
+//   arr.forEach((item, idx) => console.log(`${idx + 1} - ${item}`));
+
+// // logItem([`Mango`, `Polly`, `Ajax`]);
+// logItem([`apple`, `limon`, `banana`]);
+
+// Task-2
